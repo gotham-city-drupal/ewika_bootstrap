a6d012a0d33f8691ed088eb8b59758debc6ecb7a
Mike Pirog <mike@kalamuna.com>
2013-03-20 14:09:44 -0700

    remove isotope refs

--- js/metro-custom.js
+++ js/metro-custom.js
@@ -2,30 +2,6 @@
 	$(window)
 			.load(
 					function() {
-
-						var $container = $('.portfolio');
-						$container.isotope({
-							filter : '*',
-							animationOptions : {
-								duration : 750,
-								easing : 'linear',
-								queue : false,
-							}
-						});
-
-						$('nav.primary ul a').click(function() {
-							var selector = $(this).attr('data-filter');
-							$container.isotope({
-								filter : selector,
-								animationOptions : {
-									duration : 750,
-									easing : 'linear',
-									queue : false,
-								}
-							});
-							return false;
-						});
-
 						var $optionSets = $('nav.primary ul'), $optionLinks = $optionSets
 								.find('a');
 
